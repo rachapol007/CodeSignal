6e240dac54e67275a053b2cb3089514d10f77e33
rachapol007 <30426508+rachapol007@users.noreply.github.com>
2018-11-04 11:57:59 +0700

    Update CenturyFromYear.py

--- Arcade/Intro/CenturyFromYear.py
+++ Arcade/Intro/CenturyFromYear.py
@@ -7,7 +7,7 @@ def centuryFromYear(year):
  else:
   return (m.floor(year/100))
   
- 
+''' 
 Given a year, return the century it is in. 
 The first century spans from the year 1 up to and including the year 100, 
 the second - from the year 101 up to and including the year 200, etc.
@@ -19,3 +19,4 @@ Example:
 Test:
   Input: year: 1988
   Expected Output: 20
+  '''
