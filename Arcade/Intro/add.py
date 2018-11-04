e140009e39279ed73ec7576e897eeac22b4d8390
rachapol007 <30426508+rachapol007@users.noreply.github.com>
2018-11-04 11:54:33 +0700

    Create add.py

---
+++ Arcade/Intro/add.py
@@ -0,0 +1,21 @@
+def add(param1, param2):
+ try:
+  if param1 >= (-100) & param1 <= 1000:
+    if param2 >= (-100) & param2 <= 1000:
+   
+      return (param1+param2)
+  
+ except:
+   print ("pass")
+
+ 
+ 
+ 
+Write a function that returns the sum of two numbers.
+
+Example: 
+  For param1 = 1 and param2 = 2, the output should be add(param1, param2) = 3.
+  
+Test: 
+Input: param1: 1 ,param2: 2
+Expected Output: 3
