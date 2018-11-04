344f28678fe185f09984eb53fa3d503fa8d443f4
rachapol007 <30426508+rachapol007@users.noreply.github.com>
2018-11-04 12:07:29 +0700

    Create MakeArrayConsecutive.py

---
+++ Arcade/Edge_of_Ocean/MakeArrayConsecutive.py
@@ -0,0 +1,20 @@
+def makeArrayConsecutive2(statues):
+    count = 0
+    totla = 0
+    maix = max(statues)
+    mini = min(statues)
+    count = (maix - mini)+1
+    totla = count - len(statues)
+            
+    return  totla
+    
+    
+ '''
+ Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+Example:
+  For statues = [6, 2, 3, 8], the output should be makeArrayConsecutive2(statues) = 3.
+  Ratiorg needs statues of sizes 4, 5 and 7.
+
+'''
+    
